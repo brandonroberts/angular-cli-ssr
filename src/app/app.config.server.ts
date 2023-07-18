d24f7b844a7006e2ab03ccf9564803a54936840b
Enea Jahollari <jahollarienea14@gmail.com>
2023-07-18 18:36:13 +0000

    fix: add provideClientHydration in browser app config and not server

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,13 +1,11 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
-import { provideClientHydration } from '@angular/platform-browser';
 
 import { appConfig } from './app.config';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
-    provideClientHydration()
   ],
 };
 
